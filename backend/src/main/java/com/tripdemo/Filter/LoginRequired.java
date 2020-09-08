73ab9a1e4329bf47a222fbde2c393bfd876832b5
Jack Ma <1791670972@qq.com>
2020-09-09 00:28:49 +0800

    Filter配置跨域

--- backend/src/main/java/com/tripdemo/Filter/LoginRequired.java
+++ backend/src/main/java/com/tripdemo/Filter/LoginRequired.java
@@ -9,6 +9,7 @@ import javax.annotation.Resource;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebFilter(value = {"/indents/*",
@@ -27,6 +28,16 @@ public class LoginRequired implements Filter {
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
         HttpServletRequest request = (HttpServletRequest)req;
+        HttpServletResponse response = (HttpServletResponse) resp;
+        //根据请求头设置响应头
+        String allowOrigin = request.getHeader("Origin");
+        String allowHeader = request.getHeader("Access-Control-Request-Headers");
+        response.setHeader("Access-Control-Allow-Origin", allowOrigin);
+        //设置允许带cookie的请求
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "OPTIONS, POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", allowHeader);
+
         String tokenStr = request.getHeader("token");
         MyToken token = userService.getToken(tokenStr);
 
@@ -35,7 +46,7 @@ public class LoginRequired implements Filter {
         }
         else{
             req.setAttribute("userId", token.getUserId());
-            chain.doFilter(request, resp);
+            chain.doFilter(request, response);
         }
     }
 
